19cc5c292e445c56c60514f8a881541bd522c1e3
Rafsan Anan <anan.dhaka@gmail.com>
2020-09-19 13:47:55 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpvZwsilyQiXdLoyofvsBaNORCY8qqVOM",
+    authDomain: "burj-al-arab-c6aa4.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-c6aa4.firebaseio.com",
+    projectId: "burj-al-arab-c6aa4",
+    storageBucket: "burj-al-arab-c6aa4.appspot.com",
+    messagingSenderId: "76387214485",
+    appId: "1:76387214485:web:57b34ae321c0dc0caf279c"
+  };
+
+export default firebaseConfig;
